46f00f099a3390f24949d6b1384a9ae15fa605df
Simon Arledal (SimZor) <arledal.simon@gmail.com>
2018-10-29 01:19:04 +0100

    Design updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header>
+          <div className="container">
+            <h4>
+              Simon Arledal -&nbsp;
+              <span style={{ fontWeight: '400' }}>
+                Software Developer
+              </span>
+            </h4>
+            <hr />
+            <ul className="inline-list">
+              <li>
+                <a href="https://linkedin.com/in/simonarledal">
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/SimZor">
+                  Github
+                </a>
+              </li>
+              <li>
+                <a href="mailto:arledal.simon@gmail.com">
+                  Email
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/SimZors">
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  PGP Key
+                </a>
+              </li>
+            </ul>
+          </div>
         </header>
+        <main>
+          <div className="container">
+            <h4>Blog posts</h4>
+            <h4>Projects</h4>
+          </div>
+        </main>
+        <footer>
+          <div className="container">
+            <small>Copyright (c) Simon Arledal 2018 || You can find the source of this site on my Github.</small>
+          </div>
+        </footer>
       </div>
     );
   }
